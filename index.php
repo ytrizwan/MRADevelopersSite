5942be4489e8f2d0cd1fb8cec25ab0c56ec70df4
ytrizwan <ytrizwan@gmail.com>
2019-01-22 02:24:46 +1100

    My First Post on 22/01/2019 2AM

---
+++ index.php
@@ -0,0 +1,193 @@
+<!--<!--<!DOCTYPE html>
+<html>
+<head>
+	<title>MRA Developers</title>
+	<!--Bootstrap 4 Framwork Link and Javascript Animations for Bootstrap 4-->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+	<!--CSS Documentation Link-->
+	<link rel="stylesheet" type="text/css" href="assets/style.css">
+</head>
+
+<body class="mainBackgroundColor">
+	<div>
+		<!--Navigation Bar Start-->
+		<nav class="navbar navbar-expand-lg fixed-top navbar-dark bg-dark disableHoverTopLogo">
+			<a class="navbar-brand" href="index.php">
+				<span id="mraPart" class="logoStyleTopLogo">M<span class="flip_text yellowColorText">R</span>A <span class="flip_text yellowColorText">D</span>evelopers</span>
+			</a>
+		
+			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="true" aria-label="Toggle navigation">
+   				<span class="navbar-toggler-icon"></span>
+   			</button>
+		
+			<div class="collapse navbar-collapse" id="navbarSupportedContent">
+				<ul class="navbar-nav mr-auto">
+					<li class="nav-item active">
+						<a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+					</li>
+					<li class="nav-item dropdown">
+						<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Programming</a>
+
+						<div class="dropdown-menu" aria-labelledby="navbarDropdown">
+							<a class="dropdown-item" href="#">Action</a>
+							<a class="dropdown-item" href="#">Another action</a>
+							<div class="dropdown-divider"></div>
+							<a class="dropdown-item" href="#">Something else here</a>
+						</div>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="#">About</a>
+					</li>
+				</ul>
+				<form class="form-inline my-2 my-lg-0">
+					<input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+					<button class="btn btn-outline-warning my-2 my-sm-0" type="submit">Search</button>
+				</form>
+			</div>
+		</nav>
+
+		<!--Using Jumbotron at Top After The Header-->
+		<div class="jumbotron jumbotronOnTop jumbotronWhiteColor disableHover jumbotronBoarderHead">
+
+
+			<h1 class="display-3 text-center responsiveTextSize">Welcome to <a href="index.php">
+				
+				
+				
+				<span id="mraPart" class="logoStyle">M<span class="flip_text yellowColorText">R</span>A <span class="flip_text yellowColorText">D</span>evelopers</span>
+				
+				
+				
+				
+				</a></h1>
+			<br>
+			<div class="container">
+				<div class="row showBoarder"></div>
+			</div><br>
+			
+<!--			<hr class="my-4">-->
+
+
+		</div>
+
+
+
+
+
+
+
+
+
+
+		<!--Main Middle Block, Where I put All Elements-->
+		<div class="container mainContainer" style="margin-top: 5px;">
+			<div class="row text-center">
+				<div class="col-lg-8 col-12">
+					<div class="col-12 showBoarderWithNoRadious marginTopBottomByTen boldText">
+						Latest Posts
+					</div>
+					
+					
+					
+					
+					
+					
+					
+					
+					<div class="showBoarder eachMainPostUpperBottom lightBlueColor">
+						<div class="row showBoarder whiteColorText marginAllSidesByTen">
+							<div class="col-12">
+								This is Post Header
+							</div>
+						</div>
+
+						<div class="row marginAllSidesByTen">
+							<div class="col-lg-4 showBoarder">
+								This is Part Of Images
+							</div>
+							<div class="col-lg-1"></div>
+							<div class="col-lg-7 showBoarder whiteColorText">
+								This is Part of Text
+							</div>
+						</div>
+					</div>
+					
+					
+					
+					<div class="showBoarder eachMainPostUpperBottom lightBlueColor">
+						<div class="row showBoarder whiteColorText marginAllSidesByTen">
+							<div class="col-12">
+								This is Post Header
+							</div>
+						</div>
+
+						<div class="row marginAllSidesByTen">
+							<div class="col-lg-4 showBoarder">
+								This is Part Of Images
+							</div>
+							<div class="col-lg-1"></div>
+							<div class="col-lg-7 showBoarder whiteColorText">
+								This is Part of Text
+							</div>
+						</div>
+					</div>
+					
+					
+					
+					
+					
+					
+					
+					
+				</div>
+
+				
+
+				
+				
+				
+				<div class="col-lg-4 col-12">
+					
+					<div class="col-12 showBoarderWithNoRadious marginTopBottomByTen boldText">
+						Top Posts
+					</div>
+					
+					
+					
+					
+					
+					
+					<div class="col-12 showBoarder lightBlueColor marginTopBottomByTen">
+						<div class="row marginTopBottomByTenAndSides">
+							<div class="col-5 showBoarder">
+								This is Image
+							</div>
+							<div class="col-7 showBoarder whiteColorText">
+								This is short Text
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+
+
+
+
+
+
+
+
+
+
+		<!--Using Jumbotron as Footer-->
+		<div class="jumbotron jumbotronWhiteColor disableHover jumbotronBoarderFooter">
+			<a class="text-center" href="index.php"><h1 class="display-3" style="font-size: 3vw;"><span id="mraPart" class="logoStyle">M<span class="flip_text yellowColorText">R</span>A <span class="flip_text yellowColorText">D</span>evelopers</span></h1></a>
+		</div>
+	</div>
+</body>
+</html>
+</html>
