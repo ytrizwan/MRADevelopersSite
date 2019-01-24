b63206c7760e192b2e002a848752a867c1ca85bf
ytrizwan <ytrizwan@gmail.com>
2019-01-24 14:40:13 +1100

    Common Libraries in Template

--- index.php
+++ index.php
@@ -2,15 +2,10 @@
 <html>
 <head>
 	<title>MRA Developers</title>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<!--Bootstrap 4 Framwork Link and Javascript Animations for Bootstrap 4-->
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
-	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
-	<!--CSS Documentation Link-->
-	<link rel="stylesheet" type="text/css" href="assets/style.css">
+	<!--Common Libraries for Each Page-->
+	<?php
+	include "templates/commonTopLibraries.php";
+	?>
 </head>
 
 <body class="mainBackgroundColor">
