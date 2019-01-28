adc430dd91becb9ab12a600257046bcf2f46956d
ytrizwan <ytrizwan@gmail.com>
2019-01-28 12:01:23 +1100

    Navbar settings Changed

--- templates/navigationBarTemplate.php
+++ templates/navigationBarTemplate.php
@@ -14,19 +14,30 @@
 			<li class="nav-item active">
 				<a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
 			</li>
+			
+			<li class="nav-item">
+				<a class="nav-link" href="#">Techniques</a>
+			</li>
+			
 			<li class="nav-item dropdown">
-				<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Programming</a>
+				<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Programs</a>
 
 				<div class="dropdown-menu" aria-labelledby="navbarDropdown">
-					<a class="dropdown-item" href="#">Action</a>
-					<a class="dropdown-item" href="#">Another action</a>
-					<div class="dropdown-divider"></div>
-					<a class="dropdown-item" href="#">Something else here</a>
+					<a class="dropdown-item" href="#">Android</a>
+					<a class="dropdown-item" href="#">Java</a>
 				</div>
 			</li>
-			<li class="nav-item">
-				<a class="nav-link" href="#">About</a>
+			
+			<li class="nav-item dropdown">
+				<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Info</a>
+
+				<div class="dropdown-menu" aria-labelledby="navbarDropdown">
+					<a class="dropdown-item" href="#">About Us</a>
+					<div class="dropdown-divider"></div>
+					<a class="dropdown-item" href="#">Curriculum vitae (CV)</a>
+				</div>
 			</li>
+			
 		</ul>
 		<form class="form-inline my-2 my-lg-0">
 			<input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
