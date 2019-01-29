e2a0116baf94084c2171e9eb2de1a297a413876c
ytrizwan <ytrizwan@gmail.com>
2019-01-29 23:42:44 +1100

    Used "use strict" in JavaScript

--- assets/scripts.js
+++ assets/scripts.js
@@ -2,12 +2,14 @@
 
 		//Hide and Show Main Post Side
 		$(window).resize(function() {
+			"use strict";
 			showOrHideMainPostOnScreenSizeChange();
 		});
 		
 		showOrHideMainPostOnScreenSizeChange();
 		
 		function showOrHideMainPostOnScreenSizeChange(){
+			"use strict";
 			if($(window).width() <= 977){
 				$("#mainPost").hide();
 			}else{
