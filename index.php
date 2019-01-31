f49a80062eacfbc4e9dd131f25d14a1a760e3fda
ytrizwan <ytrizwan@gmail.com>
2019-01-31 14:12:19 +1100

    SideBar as template

--- index.php
+++ index.php
@@ -213,55 +213,15 @@
 
 
 
-				<div id="mainPost" class="col-lg-4 col-12">
-
-					<div class="col-12 showBoarderWithNoRadious marginTopBottomByTen boldText whiteTextWithBlackBackground boarderShadow">
-						Main Posts
-					</div>
-
-					<div class="col-12 showBoarder lightBlueBackground marginTopBottomByTen boarderShadow">
-
-
-
-
-
-
-						<div class="row marginTopBottomByTenAndSides">
-							<div class="col-12 showBoarder whiteColorBackground boldText boarderShadow">
-								Programming
-							</div>
-							<ul class="marginTopByFive alignTextToLeft">
-								<li>Pakistan</li>
-								<li>India</li>
-								<li>Australia</li>
-							</ul>
-						</div>
-						<hr>
-
-
-
-
-
-
-
-						<div class="row marginTopBottomByTenAndSides">
-							<div class="col-12 showBoarder whiteColorBackground boldText boarderShadow">
-								Programming
-							</div>
-							<ul class="marginTopByFive alignTextToLeft">
-								<li>Pakistan</li>
-								<li>India</li>
-								<li>Australia</li>
-							</ul>
-						</div>
-
-
-
-
-
-
-					</div>
-				</div>
+				<?php
+					include "templates/sideBarTemplate.php";
+				?>
+				
+				
+				
+				
+				
+				
 			</div>
 		</div>
 
