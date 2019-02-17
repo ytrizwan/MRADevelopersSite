2e1c33235ac75674aa410372eb0081fe08cb50ea
ytrizwan <ytrizwan@gmail.com>
2019-02-17 20:04:21 +1100

    Fixing Text

--- curriculumVitae.php
+++ curriculumVitae.php
@@ -16,14 +16,47 @@
 		?>
 
 		<!--Main Middle Block, Where I put All Elements-->
-		<div class="container mainContainer marginTopByFive showBoarderLeftRight">
-			<div class="row text-center">
-				<div class="col-4" style="background: #7543ad; padding-top: 50px; min-height: 1000px;">
-					<img class="imageStyleOfCV img-fluid rounded-circle" alt="rizwan" src="assets/images/static/rizwan.jpg">
+<!--		<div class="container mainContainer marginTopByFive showBoarderLeftRight">-->
+		<div class="container marginTopByFive showBoarderLeftRight">
+			<div class="row">
+<!--				<div class="col-md-4 text-center" style="background: #7543ad; padding-top: 50px; min-height: 1000px; color: white;">-->
+				<div class="col-lg-4 text-center" style="background: #7543ad; padding-top: 50px; color: white;">
+
+					<img class="imageStyleOfCV img-fluid" alt="rizwan" src="assets/images/static/rizwan.jpg">
+					<div style="text-align: left; margin-left: 10px;">
+						<td><h3 class="responsiveTextSizeOfTwo">CONTACT</h3></td>
+						<table style="width: 100%">
+							<tr>
+								<td class="text-center"><i class="fas fa-map-marked-alt"></i></td>
+								<td> Canberra </td>
+							</tr>
+							
+							<tr>
+								<td class="text-center"><i class="fas fa-phone"></i></td>
+								<td> +61 426 046 919 </td>
+							</tr>
+							
+							<tr>
+								<td class="text-center"><i class="fas fa-envelope"></i></td>
+								<td> ytrizwan@gmail.com </td>
+							</tr>
+						</table>
+						
+						
+						
+						
+						
+						
+						
+					</div>
+					
+					
 				</div>
 				
-				<div class="col-8 marginTopByTwenty" style="padding-top: 50px;">
-					<h1 class="brushFront">Muhammad Rizwan Asim</h1>
+				<div class="col-lg-8 marginTopByTwenty brushFront" style="padding-top: 50px; min-height: 1000px;">
+					<h1 class="reponsiveTextSizeMaxSixtyFive">Muhammad Rizwan Asim</h1>
+					<P class="reponsiveTextSizeMaxThirtyFive">Software Developer</P>
+					
 				</div>
 			</div>
 		</div>
--- templates/commonTopLibraries.php
+++ templates/commonTopLibraries.php
@@ -15,3 +15,6 @@
 
 <!--CSS Documentation Link-->
 <link rel="stylesheet" type="text/css" href="assets/style.css">
+
+<!--Link for FontAwesome-->
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
