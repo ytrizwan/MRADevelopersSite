865a6b801d3e0a99d80fc96537c264b87b49376a
ytrizwan <ytrizwan@gmail.com>
2019-02-19 14:09:07 +1100

    Complete Changes

--- curriculumVitae.php
+++ curriculumVitae.php
@@ -16,14 +16,12 @@
 		?>
 
 		<!--Main Middle Block, Where I put All Elements-->
-<!--		<div class="container mainContainer marginTopByFive showBoarderLeftRight">-->
 		<div class="container marginTopByFive showBoarderLeftRight">
 			<div class="row">
-<!--				<div class="col-md-4 text-center" style="background: #7543ad; padding-top: 50px; min-height: 1000px; color: white;">-->
-				<div class="col-lg-4 text-center" style="background: #7543ad; padding-top: 50px; color: white;">
+				<div class="col-lg-4 text-center padding_left_fifty" style="background: #7543ad; color: white;">
 
 					<img class="imageStyleOfCV img-fluid" alt="rizwan" src="assets/images/static/rizwan.jpg">
-					<div style="text-align: left; margin-left: 10px;">
+					<div class="text_Align_Left" style="margin-left: 10px;">
 						<td><h3 class="responsiveTextSizeOfTwo">CONTACT</h3></td>
 						<table style="width: 100%">
 							<tr>
@@ -53,36 +51,36 @@
 					
 				</div>
 				
-				<div class="col-lg-8 marginTopByTwenty brushFront" style="padding-top: 50px; min-height: 1000px;">
+				<div class="col-lg-8 marginTopByTwenty brushFront padding_left_fifty" style="min-height: 1000px;">
 					<h1 class="reponsiveText_Size_CV_Name">Muhammad Rizwan Asim</h1>
 					<P class="reponsiveText_Size_MaxFifteen_MinSeventeenVM">Software Developer</P>
-					<div style="background: #e4e4e4; border-bottom-left-radius: 50px; border-top-left-radius: 50px">
+					<div class="label_Style_CV">
 						<table>
 							<tr>
 								<td>
-									<i class="fas fa-sign fa-2x" style="border-radius: 50%;padding: 10px; background: white;"></i>
+									<i class="fas fa-sign fa-2x icon_Style_CV"></i>
 								</td>
 								
 								<td>
-									<span class="reponsiveText_Size_MaxThirtyEight_MinFourVM" style="font-weight: bold;"> OBJECTIVE</span>
+									<span class="reponsiveText_Size_MaxThirtyEight_MinFourVM bold_Text"> OBJECTIVE</span>
 								</td>
 							</tr>
 						</table>
 					</div>
 					
-					<p class="reponsiveTextSizeMaxEighteen" style="text-align: justify; padding-left: 50px;">To be a part of an organization that provides an atmosphere of mutual growth and training, where I can show my talent and potential while applying my Computer Sciences, Information Technology qualifications and Interpersonal skills.</p>
+					<p class="reponsiveTextSizeMaxEighteen padding_left_fifty text_Align_Justify">To be a part of an organization that provides an atmosphere of mutual growth and training, where I can show my talent and potential while applying my Computer Sciences, Information Technology qualifications and Interpersonal skills.</p>
 					
 					
 					
-					<div style="background: #e4e4e4; border-bottom-left-radius: 50px; border-top-left-radius: 50px">
+					<div class="label_Style_CV">
 						<table>
 							<tr>
 								<td>
-									<i class="fas fa-graduation-cap fa-2x" style="border-radius: 50%; padding: 10px; background: white;"></i>
+									<i class="fas fa-graduation-cap fa-2x icon_Style_CV"></i>
 								</td>
 								
 								<td>
-									<span class="reponsiveText_Size_MaxThirtyEight_MinFourVM" style="font-weight: bold;"> ACADEMIC ACHIEVEMENTS </span>
+									<span class="reponsiveText_Size_MaxThirtyEight_MinFourVM bold_Text"> ACADEMIC ACHIEVEMENTS </span>
 								</td>
 							</tr>
 						</table>
@@ -90,12 +88,12 @@
 					
 					<div class="row">
 						<div class="col-md-4 col-sm-12">
-							<span class="reponsiveText_Size_MaxEighteen_MinOneVW" style="text-align: justify; padding-left: 50px;">July 2018 - Dec 2018</span>
+							<span class="reponsiveText_Size_MaxEighteen_MinOneVW padding_left_fifty text_Align_Justify">July 2018 - Dec 2018</span>
 						</div>
 						
 						<div class="col-md-8 col-sm-12" style="float: left;">
-							<span class="reponsiveText_Size_MaxEighteen_MinOneVW" style="float: left; text-align: left; padding-left: 50px;">Diploma of Software Development</span><br>
-							<span class="reponsiveText_Size_MaxEighteen_MinOneVW" style="float: left; text-align: left; padding-left: 50px; font-weight: bold; color: #7E7E7E;">Canberra Institute Of Technology (CIT)</span>
+							<span class="reponsiveText_Size_MaxEighteen_MinOneVW text_Align_Left float_TO_Left padding_left_fifty">Diploma of Software Development</span><br>
+							<span class="reponsiveText_Size_MaxEighteen_MinOneVW text_Align_Left float_TO_Left padding_left_fifty weight_bold oolor_White_Gray">Canberra Institute Of Technology (CIT)</span>
 						</div>
 					</div>
 					
@@ -108,12 +106,14 @@
 					
 					<div class="row">
 						<div class="col-md-4 col-sm-12">
-							<span class="reponsiveText_Size_MaxEighteen_MinOneVW" style="text-align: justify; padding-left: 50px;">Sep 2012 - Sep 2016</span>
+							<span class="reponsiveText_Size_MaxEighteen_MinOneVW padding_left_fifty text_Align_Justify">Sep 2012 - Sep 2016</span>
 						</div>
 						
 						<div class="col-md-8 col-sm-12" style="float: left;">
-							<span class="reponsiveText_Size_MaxEighteen_MinOneVW" style="float: left; text-align: left; padding-left: 50px;">Bachelor of Science in Computer Science (4 Year Program)</span><br>
-							<span class="reponsiveText_Size_MaxEighteen_MinOneVW" style="float: left; text-align: left; padding-left: 50px; font-weight: bold; color: #7E7E7E;">Virtual University of Pakistan (VU)</span>
+							<span class="reponsiveText_Size_MaxEighteen_MinOneVW text_Align_Left float_TO_Left padding_left_fifty">
+									Bachelor of Science in Computer Science (4 Year Program)
+								 </span><br>
+							<span class="reponsiveText_Size_MaxEighteen_MinOneVW text_Align_Left float_TO_Left padding_left_fifty weight_bold oolor_White_Gray">Virtual University of Pakistan (VU)</span>
 						</div>
 					</div>
 					<div class="container"><hr></div>
@@ -124,12 +124,12 @@
 					
 					<div class="row">
 						<div class="col-md-4 col-sm-12">
-							<span class="reponsiveText_Size_MaxEighteen_MinOneVW" style="text-align: justify; padding-left: 50px;">2008 - 2010</span>
+							<span class="reponsiveText_Size_MaxEighteen_MinOneVW padding_left_fifty text_Align_Justify">2008 - 2010</span>
 						</div>
 						
 						<div class="col-md-8 col-sm-12" style="float: left;">
-							<span class="reponsiveText_Size_MaxEighteen_MinOneVW" style="float: left; text-align: left; padding-left: 50px;">Intermediate</span><br>
-							<span class="reponsiveText_Size_MaxEighteen_MinOneVW" style="float: left; text-align: left; padding-left: 50px; font-weight: bold; color: #7E7E7E;">Government College Township Lahore, Pakistan</span>
+							<span class="reponsiveText_Size_MaxEighteen_MinOneVW text_Align_Left float_TO_Left padding_left_fifty">Intermediate</span><br>
+							<span class="reponsiveText_Size_MaxEighteen_MinOneVW text_Align_Left float_TO_Left padding_left_fifty weight_bold oolor_White_Gray">Government College Township Lahore, Pakistan</span>
 						</div>
 					</div>
 					<div class="container"><hr></div>
@@ -164,15 +164,15 @@
 					
 					
 					
-					<div style="background: #e4e4e4; border-bottom-left-radius: 50px; border-top-left-radius: 50px">
+					<div class="label_Style_CV">
 						<table>
 							<tr>
 								<td>
-									<i class="fas fa-sign fa-2x" style="border-radius: 50%;padding: 10px; background: white;"></i>
+									<i class="fas fa-trophy fa-2x icon_Style_CV"></i>
 								</td>
 								
 								<td>
-									<span class="reponsiveText_Size_MaxThirtyEight_MinFourVM" style="font-weight: bold;"> OBJECTIVE</span>
+									<span class="reponsiveText_Size_MaxThirtyEight_MinFourVM bold_Text"> ACHIEVEMENTS</span>
 								</td>
 							</tr>
 						</table>
