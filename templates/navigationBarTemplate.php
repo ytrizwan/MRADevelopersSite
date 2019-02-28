8f6a0aac2327a127518c2bcb73d9275a1a083f24
ytrizwan <ytrizwan@gmail.com>
2019-02-28 12:27:20 +1100

    Start Working on Search

--- templates/navigationBarTemplate.php
+++ templates/navigationBarTemplate.php
@@ -42,8 +42,8 @@
 			</li>
 			
 		</ul>
-		<form class="form-inline my-2 my-lg-0">
-			<input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+		<form class="form-inline my-2 my-lg-0" action="search.php" method="get">
+			<input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" name="searchInput">
 			<button class="btn btn-light my-2 my-sm-0" type="submit">Search</button>
 		</form>
 	</div>
